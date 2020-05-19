b9a5704ee82ed3c40c42c9e9f734bd6399fe56f1
isaevDaniil <isaev.daniil48@yandex.ru>
2020-05-19 17:02:39 +0300

    rg3

--- mod5-sol/js/script.js
+++ mod5-sol/js/script.js
@@ -97,7 +97,6 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
@@ -116,7 +115,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage = insertProperty(home-snippet,"randomCategoryShortName",chosenCategoryShortName);
+      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName","\'" + chosenCategoryShortName + "\'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
